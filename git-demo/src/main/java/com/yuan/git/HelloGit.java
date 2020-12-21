2e2ec13c3e08d2fcbb1aaa46aad5ddb66898704f
weiti <1415345410@qq.com>
2020-12-22 01:40:45 +0800

    add git-demo

---
+++ git-demo/src/main/java/com/yuan/git/HelloGit.java
@@ -0,0 +1,18 @@
+package com.yuan.git;
+
+/**
+ * @author zyl
+ * @create 2020-12-22 1:30
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
